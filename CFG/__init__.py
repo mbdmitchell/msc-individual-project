99abefb9536a414da862caf867cbf60a3c4ae20b
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-06-03 12:12:00 +0100

    Add example multiedge cfg

--- CFG/__init__.py
+++ CFG/__init__.py
@@ -2,4 +2,8 @@ __version__ = "1.0.0"
 __author__ = "Max Mitchell"
 
 from .CFG import *
-from .example_CFGs import example_cfg_1, example_cfg_2, example_cfg_3
+from .example_CFGs \
+    import example_cfg_if_else, \
+    example_cfg_while_loop, \
+    example_cfg_switch_while_combo, \
+    example_cfg_multigraph
--- CFG/example_CFGs.py
+++ CFG/example_CFGs.py
@@ -15,26 +15,29 @@ def _generate_multi_di_graph_from_edges(edges) -> nx.MultiDiGraph:
     return g
 
 
-def example_cfg_1() -> CFG:
+def example_cfg_if_else() -> CFG:
     """Return if-else CFG"""
     g = _generate_multi_di_graph_from_edges([(1, 2), (1, 3), (2, 4), (3, 4)])
     return CFG.CFG(graph=g)
 
 
-def example_cfg_2() -> CFG:
+def example_cfg_while_loop() -> CFG:
     """Return while CFG"""
     g = _generate_multi_di_graph_from_edges([(1, 2), (2, 4), (2, 3), (3, 2)])
     return CFG.CFG(graph=g)
 
 
-def example_cfg_3() -> CFG:
+def example_cfg_switch_while_combo() -> CFG:
     """Return combo_all_cfs CFG"""
     g = _generate_multi_di_graph_from_edges([(1, 2), (1, 3), (2, 8), (2, 4), (3, 5), (3, 6), (3, 7),
                                              (4, 2), (5, 9), (6, 9), (7, 9), (8, 10), (9, 10)])
     return CFG.CFG(graph=g)
 
+def example_cfg_multigraph() -> CFG:
+    g = _generate_multi_di_graph_from_edges([(1, 2), (1, 2), (1, 2), (1, 3)])
+    return CFG.CFG(graph=g)
 
 if __name__ == '__main__':
-    cfgs = [example_cfg_1(), example_cfg_2(), example_cfg_3()]
+    cfgs = [example_cfg_if_else(), example_cfg_while_loop(), example_cfg_switch_while_combo()]
     for ix, cfg in enumerate(cfgs):
         cfg.save_as(f'test{ix}.pickle', CFG.GraphFormat.CFG)
