47c5313cc85e1cf8127dfabd6f7b56aee9d8d6a8
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-06-09 18:06:19 +0100

    Add example CFG

--- CFG/example_CFGs.py
+++ CFG/example_CFGs.py
@@ -33,11 +33,22 @@ def example_cfg_switch_while_combo() -> CFG:
                                              (4, 2), (5, 9), (6, 9), (7, 9), (8, 10), (9, 10)])
     return CFG.CFG(graph=g)
 
+
 def example_cfg_multigraph() -> CFG:
     g = _generate_multi_di_graph_from_edges([(1, 2), (1, 2), (1, 2), (1, 3)])
     return CFG.CFG(graph=g)
 
-if __name__ == '__main__':
-    cfgs = [example_cfg_if_else(), example_cfg_while_loop(), example_cfg_switch_while_combo()]
-    for ix, cfg in enumerate(cfgs):
-        cfg.save_as(f'test{ix}.pickle', CFG.GraphFormat.CFG)
+
+def example_cfg_misc() -> CFG:
+    g = _generate_multi_di_graph_from_edges([
+        (1, 2),
+        (2, 3), (2, 6), (2, 2),
+        (3, 4),
+        (4, 5), (4, 6), (4, 3),
+        (5, 7), (5, 8),
+        (6, 9),
+        (7, 9), (7, 3),
+        (8, 9), (8, 9), (8, 9)
+    ])
+
+    return CFG.CFG(graph=g)
