1032399b84fa59e88d75aba3978079a5b2212627
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-07-14 23:57:40 +0100

    Add util functions for WAT package

--- WAT/utils.py
+++ WAT/utils.py
@@ -1,16 +1,20 @@
 import subprocess
+import os
 
-
-def optimise(unoptimised_wasm_filepath: str, opt_option: str, verbose=False):
+def optimise_wasm(unoptimised_wasm_filepath: str, opt_option: str, verbose=False):
     """Generate optimised wasm binary"""
 
     if opt_option not in {"O", "O1", "O2", "O3", "O4", "Os", "Oz"}:
         raise ValueError("Invalid opt_option")
 
-    output_filepath = f"./opt{opt_option}.wasm"
+    # Extract directory from the input file path
+    directory = os.path.dirname(unoptimised_wasm_filepath)
+
+    # Generate the output file path in the same directory as the input file
+    output_filepath = os.path.join(directory, f"opt{opt_option}.wasm")
 
     command = [
-        "/opt/homebrew/Cellar/binaryen/117/bin/wasm-opt",  # TODO <- eww
+        "/opt/homebrew/Cellar/binaryen/117/bin/wasm-opt",
         "--enable-multimemory",
         unoptimised_wasm_filepath,
         "-o",
@@ -25,3 +29,4 @@ def optimise(unoptimised_wasm_filepath: str, opt_option: str, verbose=False):
             print(f"Optimization completed successfully. Output file: {output_filepath}")
         else:
             print(f"Optimization failed with return code {result.returncode}")
+            print(result.stderr)
