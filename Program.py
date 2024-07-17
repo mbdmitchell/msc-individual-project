1a96e3ea80906e92f72da04ba7c2a84dbf8e6f0b
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-07-17 12:40:41 +0100

    Refactor

--- Program.py
+++ Program.py
@@ -20,37 +20,3 @@ class Program(ABC):
             return existing_code == self._code
         except FileNotFoundError:
             return False
-
-    @staticmethod
-    @abstractmethod
-    def compile(source_code_path, target_binary_path):
-        pass
-
-    def save(self, file_path: str, save_as_executable: bool, verbose=False):
-
-        directory = os.path.dirname(file_path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        source_code_path = f"{file_path}.wat"
-        binary_path = f"{file_path}.wasm"
-
-        source_code_file_already_existed: bool = os.path.exists(source_code_path)
-        up_to_date: bool = self._is_up_to_date(source_code_path)
-
-        if not save_as_executable:
-            with open(source_code_path, "w") as file:
-                file.write(self.get_code())
-
-        else:
-            if not source_code_file_already_existed or not up_to_date:
-                with open(source_code_path, "w") as file:
-                    file.write(self.get_code())
-
-            self.compile(source_code_path, binary_path)
-
-            if not source_code_file_already_existed:
-                os.remove(source_code_path)
-
-        if verbose:
-            print("Saved to {path}".format(path=binary_path if save_as_executable else source_code_path))
--- WAT/WebAssemblyProgram.py
+++ WAT/WebAssemblyProgram.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from Program import Program
 import CFG
@@ -14,4 +15,31 @@ class WebAssemblyProgram(Program):
     def compile(source_code_path, target_binary_path):
         subprocess.run(["wat2wasm", "--enable-multi-memory", source_code_path, "-o", target_binary_path])
 
-    # see Program class for rest of functions
+    def save(self, file_path: str, save_as_executable: bool, verbose=False):
+
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        source_code_path = f"{file_path}.wat"
+        binary_path = f"{file_path}.wasm"
+
+        source_code_file_already_existed: bool = os.path.exists(source_code_path)
+        up_to_date: bool = self._is_up_to_date(source_code_path)
+
+        if not save_as_executable:
+            with open(source_code_path, "w") as file:
+                file.write(self.get_code())
+
+        else:
+            if not source_code_file_already_existed or not up_to_date:
+                with open(source_code_path, "w") as file:
+                    file.write(self.get_code())
+
+            self.compile(source_code_path, binary_path)
+
+            if not source_code_file_already_existed:
+                os.remove(source_code_path)
+
+        if verbose:
+            print("Saved to {path}".format(path=binary_path if save_as_executable else source_code_path))
