7366a5f746336d34df352c0247ac1fe102cacf9e
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-07-17 12:37:51 +0100

    Create WGSLProgram.py

---
+++ WGSL/WGSLProgram.py
@@ -0,0 +1,24 @@
+import os
+
+from Program import Program
+import CFG
+from WGSL import WGSLCodeBuilder
+
+class WGSLProgram(Program):
+    def __init__(self, cfg: CFG):
+        super().__init__(cfg)
+        self.builder = WGSLCodeBuilder.WGSLCodeBuilder(cfg)
+        self._code = self.builder.build_code()
+
+    def save(self, file_path, verbose=False):
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        source_code_path = f"{file_path}.wgsl"
+
+        with open(source_code_path, "w") as file:
+            file.write(self.get_code())
+
+        if verbose:
+            print("Saved to {path}".format(path=source_code_path))
