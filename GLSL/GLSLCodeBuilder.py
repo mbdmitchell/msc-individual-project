d0bdc80e4e4e64814cc2392d4640d0d7f614ccf1
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-07-29 11:28:45 +0100

    Fix shadertrap typo (whoops)

--- GLSL/GLSLCodeBuilder.py
+++ GLSL/GLSLCodeBuilder.py
@@ -19,12 +19,12 @@ class GLSLCodeBuilder(CodeBuilder):
 
         layout(local_size_x=1, local_size_y=1, local_size_z=1) in;
         
-        layout(std430, binding = 0) buffer inputData {{
-          uint directions[];
+        layout(std430, binding = 0) buffer directions {{
+          uint  inputData[];
         }};
         
-        layout(std430, binding = 1) buffer outputData {{
-          uint actual_path[];
+        layout(std430, binding = 1) buffer actual_path {{
+          uint outputData[];
         }};
         
         void main() {{
