570500fdd014136f117f4169e5d8dd811559bb56
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-07-29 21:21:31 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith('#')]
+
+setup(
+    name='msc-control-flow-fleshing-project',
+    version='0.1',
+    author='Max Mitchell',
+    author_email='mbm22@doc.ic.ac.uk',
+    description='A project testing compilers with control flow fleshing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mbdmitchell/msc-individual-project',
+    py_modules=['CFG', 'common', 'GLSL', 'runner', 'test', 'testing', 'WASM', 'WGSL'],
+    python_requires='>=3.9',
+    install_requires=parse_requirements('requirements.txt'),
+    test_suite='test',
+)
