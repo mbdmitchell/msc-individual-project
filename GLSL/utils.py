ddd04f779263ffd9f1e5e24878c4f1d58edb9095
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-07-29 14:08:41 +0100

    Implement run_glsl

--- GLSL/utils.py
+++ GLSL/utils.py
@@ -1,4 +1,23 @@
-def test_glsl(code_filepath, directions_path, output_path):
-    """generate shadertrap file, execute test
-        IMPORTANT: the shadertrap repo must be built"""
-    return True, ''
+from GLSL import GLSLProgram
+
+import json
+import subprocess
+import tempfile
+
+
+def run_glsl(program: GLSLProgram, input_directions: list[int]) -> bool:
+
+    expected_path = program.cfg.expected_output_path(input_directions)
+    shadertrap_test: str = program.generate_shader_test(input_directions, expected_path)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.shadertrap') as temp_file:
+        temp_file.write(shadertrap_test.encode('utf-8'))
+        shadertrap_test_filepath = temp_file.name
+
+    with open('../config.json', 'r') as config_file:
+        config = json.load(config_file)
+        shadertrap_exe_filepath = config['SHADERTRAP_PATH']
+
+    result = subprocess.run([shadertrap_exe_filepath, shadertrap_test_filepath], capture_output=True, text=True)
+
+    return result.returncode == 0
