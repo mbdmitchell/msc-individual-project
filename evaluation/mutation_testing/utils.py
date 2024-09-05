a5ae9542a16517bc5c58d8ae3b5cb4031153a7b0
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-09-05 15:32:55 +0100

    Throw errors instead of print()

--- evaluation/mutation_testing/utils.py
+++ evaluation/mutation_testing/utils.py
@@ -1,9 +1,19 @@
+import os
 import subprocess
+from pathlib import Path
+from typing import Optional
+
+from my_common import load_repo_paths_config
 
 
-def get_visitable_mutant_ids():
+def get_visitable_mutant_ids() -> list[int]:
+
+    # this prevents "File not found" error
+    current_dir = Path(__file__).parent
+    file_path = current_dir / 'visitable_mutants.txt'
+
     try:
-        with open('./visitable_mutants.txt', 'r') as file:
+        with open(file_path, 'r') as file:
             lines = file.readlines()
     except FileNotFoundError:
         raise AssertionError("'./visitable_mutants.txt' does not exist.")
@@ -11,24 +21,28 @@ def get_visitable_mutant_ids():
     if not lines:
         raise AssertionError("'./visitable_mutants.txt' is empty.")
 
-    return [int(line.strip()) for line in lines]
+    ids = [int(line.strip()) for line in lines]
 
-def get_non_visitable_mutant_ids():
+    return list(set(ids))  # removes duplicates
+
+
+def get_non_visitable_mutant_ids() -> list[int]:
 
     visitable = set(get_visitable_mutant_ids())
     largest_mutant_id = get_largest_mutant_id()
 
     if largest_mutant_id is None:
-        print("Error: Could not retrieve the largest mutant ID.")
-        return None
+        raise LookupError("Could not retrieve the largest mutant ID.")
 
     not_visitable = [i for i in range(largest_mutant_id + 1) if i not in visitable]
 
     return not_visitable
 
-def get_largest_mutant_id():
+
+def get_largest_mutant_id() -> int:
     command = ["python3", "/Users/maxmitchell/dredd/scripts/query_mutant_info.py",
-               "/Users/maxmitchell/Documents/msc-control-flow-fleshing-project/evaluation/mutation_testing/mutant_info_mutant_tracking.json",
+               "/Users/maxmitchell/Documents/msc-control-flow-fleshing-project/evaluation/mutation_testing"
+               "/mutant_info_mutant_tracking.json",
                "--largest-mutant-id"]
     result = subprocess.run(command, capture_output=True, text=True)
 
@@ -37,8 +51,7 @@ def get_largest_mutant_id():
             largest_mutant_id = int(result.stdout.strip())
             return largest_mutant_id
         except ValueError:
-            print("Error: Could not parse the largest mutant ID.")
-            return None
+            raise ValueError("Could not parse the largest mutant ID.")
     else:
-        print("Error: Script did not run successfully.")
-        return None
+        raise RuntimeError("Error: Script did not run successfully.")
+
