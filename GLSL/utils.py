f4ffc68a998a35e1e379f013a3c974a6e94858a4
Max Mitchell <maxmitchell.mm@googlemail.com>
2024-09-06 15:36:39 +0100

    Revert temp debugging changes

--- GLSL/utils.py
+++ GLSL/utils.py
@@ -8,13 +8,12 @@ def run_glsl(program: GLSLProgram, input_directions: list[int], config) -> (bool
     expected_path: list[int] = program.cfg.expected_output_path(input_directions)
     shadertrap_test: str = program.generate_shader_test(input_directions, expected_path)
 
-    permanent_filepath = '/homes/mbm22/Documents/modules/msc-project/msc-individual-project/test.shadertrap'
-
-    with open(permanent_filepath, 'w', encoding='utf-8') as file:
-        file.write(shadertrap_test)
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.shadertrap') as temp_file:
+        temp_file.write(shadertrap_test.encode('utf-8'))
+        shadertrap_test_filepath = temp_file.name
 
     shadertrap_exe_filepath = config['SHADERTRAP_PATH']
 
-    result = subprocess.run([shadertrap_exe_filepath, permanent_filepath], capture_output=True, text=True)
+    result = subprocess.run([shadertrap_exe_filepath, shadertrap_test_filepath], capture_output=True, text=True)
 
     return result.returncode == 0, result.stderr
